0f4026e810634d1ef1ee876160295d486debbf6b
Harshit Chaubey <97113356+chaubey5439@users.noreply.github.com>
2025-07-09 21:01:17 +0530

    Update poster_down.py

--- poster_down.py
+++ poster_down.py
@@ -8,17 +8,21 @@ def download_posters(indexes, df, api_key):
         title = df.loc[i]['name']
         imdb_id = df.loc[i]['movie_id']
 
-        api_url = api_url = f"http://www.omdbapi.com/?t={title}&apikey={api_key}"
+        api_url = f"http://www.omdbapi.com/?t={title}&apikey={api_key}"
         response = requests.get(api_url)
         data = response.json()
 
         if data.get("Response") == "True" and "Poster" in data:
             poster_url = data["Poster"]
+            rating = data.get("imdbRating", "N/A")
+            genre = data.get("Genre", "N/A")
+            info = f"⭐ {rating} | 🎭 {genre}"
+
             img_data = requests.get(poster_url).content
             file_path = f"posters/{title}.jpg"
             with open(file_path, "wb") as handler:
                 handler.write(img_data)
-            posters.append((title, file_path))
+            posters.append((title, file_path, info))
         else:
-            posters.append((title, None))
+            posters.append((title, None, "IMDb data not found"))
     return posters
